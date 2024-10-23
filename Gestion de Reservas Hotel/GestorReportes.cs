faca95d931f91cae08cb884b085937a329d354d0
Fernando <fernandobonilla832@gmail.com>
2024-10-23 18:07:58 -0300

    Agregado metodo ListarHabitaciones en Modulo Estadisticas y Reportes

--- Gestion de Reservas Hotel/GestorReportes.cs
+++ Gestion de Reservas Hotel/GestorReportes.cs
@@ -23,6 +23,49 @@ namespace Gestion_de_Reservas_Hotel
 
         public static void ListarHabitacionesDisponibles() 
         {
+            // Pidiendo la fecha de Check-in
+            Console.WriteLine("Ingrese la Fecha de Check-In (formato: dd/MM/yyyy):");
+
+            DateTime fechaCheckIn;
+            bool success = DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out fechaCheckIn);
+            while (!success || fechaCheckIn < DateTime.Today)
+            {
+                if (!success)
+                {
+                    Console.WriteLine("Formato de fecha inválido. Ingrese nuevamente en formato dd/MM/yyyy:");
+                }
+                else
+                {
+                    Console.WriteLine("La fecha de Check-In no puede ser anterior al día de hoy. Inténtelo de nuevo:");
+                }
+                success = DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out fechaCheckIn);
+            }
+
+            // Pidiendo la fecha de Check-Out
+            Console.WriteLine("Ingrese la fecha de Check-Out (formato: dd/MM/yyyy):");
+            DateTime fechaCheckOut;
+            success = DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out fechaCheckOut);
+            while (!success || fechaCheckOut <= fechaCheckIn)
+            {
+                if (!success)
+                {
+                    Console.WriteLine("Formato de fecha inválido. Ingrese nuevamente en formato dd/MM/yyyy:");
+                }
+                else
+                {
+                    Console.WriteLine("La fecha de Check-Out debe ser posterior a la fecha de Check-In. Inténtelo de nuevo:");
+                }
+                success = DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out fechaCheckOut); //el metodo TryParseExact obliga al usuario a que ingrese formato "dd/MM/yyyy", y luego dos paramentros 1- IFormatProvider = null, para la cultura, y 2- DateTimeStyles = None, controla cómo se deben interpretar las fechas (si se permiten espacios en blanco)
+            }
+
+            foreach (Habitacion habitacion in GestorHabitaciones.habitaciones)
+            {
+                if (GestorHabitaciones.CheckStatusHabitacion(habitacion.NumHabitacion, fechaCheckIn, fechaCheckOut) == "Disponible")
+                {
+                    Console.WriteLine(habitacion.ToString());
+                }
+            }
+
 
         }
 
